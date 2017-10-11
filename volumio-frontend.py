b39deecbc18dbc50754635b7f1fc2301f9c2f43b
Thomas Etcheverria <tetcheve@gmail.com>
2017-10-11 15:04:13 +0200

    volumio control from command line at home

---
+++ volumio-frontend.py
@@ -0,0 +1,43 @@
+#!/usr/local/bin/python2
+from mpd import	MPDClient
+import sys
+
+# connect to server
+client = MPDClient()
+server="192.168.1.221"
+client.connect(server,6600)
+
+# parse arguments
+
+if len(sys.argv)>2 or len(sys.argv)==1:
+    sys.exit(0)
+elif len(sys.argv)==2:
+    command = sys.argv[1]
+    
+    
+# switch between case
+if command=="play":
+    client.play()
+if command=="pause":
+    client.pause()
+if command=="stop":
+    client.stop()
+if command=="next":
+    client.next()
+if command=="previous":
+    client.previous()
+if command=="clear":
+    client.pause()
+    client.clear()
+if command[0]=="v":
+    option=command[1:]
+    client.pause()
+    client.clear()
+    client.load(option)
+    client.play()
+if command=="list":
+    L = client.listplaylists()
+    output = ""
+    for _ in L:
+        output+=_["playlist"] + " $ "
+    print(output)
